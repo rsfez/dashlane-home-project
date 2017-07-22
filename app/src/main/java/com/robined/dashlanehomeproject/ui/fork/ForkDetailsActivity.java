2d20194617ad1eed184a4b0e16fad5532c94a922
Robin <rsfez@users.noreply.github.com>
2017-07-22 17:04:15 +0200

    Prepared popup activity for details for tablet, normal activity for phones

---
+++ app/src/main/java/com/robined/dashlanehomeproject/ui/fork/ForkDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.robined.dashlanehomeproject.ui.fork;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import com.robined.dashlanehomeproject.R;
+
+public class ForkDetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fork_details_activity);
+    }
+}
--- app/src/main/java/com/robined/dashlanehomeproject/ui/fork/views/ForksRecyclerAdapter.java
+++ app/src/main/java/com/robined/dashlanehomeproject/ui/fork/views/ForksRecyclerAdapter.java
@@ -1,15 +1,21 @@
 package com.robined.dashlanehomeproject.ui.fork.views;
 
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import com.robined.dashlanehomeproject.ui.fork.contracts.ForkPresenter;
+import android.widget.ImageView;
+import android.widget.TextView;
 import com.robined.dashlanehomeproject.R;
+import com.robined.dashlanehomeproject.ui.fork.ForkDetailsActivity;
+import com.robined.dashlanehomeproject.ui.fork.contracts.ForkPresenter;
+import com.robined.dashlanehomeproject.ui.fork.contracts.ForkRowView;
 import com.squareup.picasso.Picasso;
 
-public class ForksRecyclerAdapter extends RecyclerView.Adapter<ForkViewHolder> {
+public class ForksRecyclerAdapter extends RecyclerView.Adapter<ForksRecyclerAdapter.ForkViewHolder> {
     private final ForkPresenter mForkPresenter;
     private final Picasso mPicasso;
 
@@ -33,5 +39,37 @@ public class ForksRecyclerAdapter extends RecyclerView.Adapter<ForkViewHolder> {
     @Override
     public int getItemCount() {
         return mForkPresenter.getForkCount();
+    }
+
+    final class ForkViewHolder extends RecyclerView.ViewHolder implements ForkRowView,
+            OnClickListener {
+        private final Picasso mPicasso;
+
+        private final TextView mOwnerNameTextView;
+        private final ImageView mOwnerImageView;
+
+        ForkViewHolder(View itemView, Picasso picasso) {
+            super(itemView);
+            mPicasso = picasso;
+            mOwnerNameTextView = itemView.findViewById(R.id.owner_name_textview);
+            mOwnerImageView = itemView.findViewById(R.id.owner_avatar_imageview);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(view.getContext(), ForkDetailsActivity.class);
+            view.getContext().startActivity(intent);
+        }
+
+        @Override
+        public void setOwnerName(String ownerName) {
+            mOwnerNameTextView.setText(ownerName);
+        }
+
+        @Override
+        public void displayPictureFromUrl(String avatarUrl) {
+            mPicasso.load(avatarUrl).into(mOwnerImageView);
+        }
     }
 }
